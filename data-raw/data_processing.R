5cfb9ede8990cbe30824a00e025e1d565def28f4
Mian <mzhong@ethz.ch>
2023-07-10 14:30:28 +0200

    move things around and update author information

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,3 +1,32 @@
 ## code to prepare `data_processing` dataset goes here
 
-usethis::use_data(data_processing, overwrite = TRUE)
+# description -------------------------------------------------------------
+
+# A script to read a KML file and extract the long and lat coordinates for
+# further processing
+
+# libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(tidyr)
+library(sf)
+library(purrr)
+library(readr)
+library(stringr)
+
+# read data ---------------------------------------------------------------
+
+locations <- read_sf("./data-raw/GPS marked skips in Blantyre.kml")
+
+st_bbox(locations)
+
+wasteskipsblantyre <- locations %>% 
+  mutate(long = unlist(map(locations$geometry, 1)),
+         lat = unlist(map(locations$geometry, 2))) %>% 
+  st_drop_geometry() %>% 
+  select(-Description) %>% 
+  
+  # enrich data
+  mutate(capacity = 7000)
+
+usethis::use_data(wasteskipsblantyre, overwrite = TRUE)
